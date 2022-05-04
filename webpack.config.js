5f2ccd87ef0a37b037cbdb683befbd5a4e91efce
ting-prado <ethel.prado1998@gmail.com>
2022-05-04 10:33:27 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/main.js',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime', new ESLintPlugin()],
+          },
+        },
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+};
