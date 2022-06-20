d646165fc50b5a46bef7b0f2c896db22a6e02df5
ting-prado <ethel.prado1998@gmail.com>
2022-06-20 12:38:24 +0800

    Added brackets

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime', new ESLintPlugin()],
+            plugins: [['@babel/plugin-transform-runtime', new ESLintPlugin()]],
           },
         },
       },
