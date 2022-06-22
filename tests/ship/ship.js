433e03de5a64d45b46b32aaf6fe992b15df1b5b1
ting-prado <ethel.prado1998@gmail.com>
2022-06-22 13:38:03 +0800

    passed all tests for ships with different length

--- tests/ship/ship.js
+++ tests/ship/ship.js
@@ -1,7 +1,28 @@
+/* eslint-disable no-plusplus */
 const Ship = (length) => {
+  const pos = ['[5, 1]', '[5, 2]', '[5, 3]', '[5, 4]'];
+  const hitmarks = [];
+
   const getLength = () => length;
 
-  return { getLength };
+  const hit = (coord) => {
+    const index = pos.indexOf(coord);
+    hitmarks[index] = 'x';
+  };
+
+  // eslint-disable-next-line consistent-return
+  const isSunk = () => {
+    let count = 0;
+    hitmarks.forEach((mark) => {
+      if (mark === 'x') {
+        count++;
+      }
+    });
+
+    if (count === getLength()) return true;
+  };
+
+  return { getLength, hit, isSunk };
 };
 
 module.exports = Ship;
--- tests/ship/ship.test.js
+++ tests/ship/ship.test.js
@@ -1,7 +1,96 @@
 /* eslint-disable no-undef */
 const Ship = require('./ship');
 
-test('sample', () => {
-  const sample = Ship(4);
-  expect(sample.getLength(4)).toEqual(4);
+describe('ship with length 4', () => {
+  let ship;
+  beforeAll(() => {
+    ship = Ship(4);
+  });
+
+  test('get length', () => {
+    expect(ship.getLength()).toBe(4);
+  });
+
+  test('does ship sink when hit at loc 1', () => {
+    ship.hit('[5, 1]');
+    expect(ship.isSunk()).toBeFalsy();
+  });
+
+  test('hit ship again at loc 4', () => {
+    ship.hit('[5, 4]');
+    expect(ship.isSunk()).toBeFalsy();
+  });
+
+  test('is ship sunk when all are hit', () => {
+    ship.hit('[5, 2]');
+    ship.hit('[5, 3]');
+    expect(ship.isSunk()).toBeTruthy();
+  });
+});
+
+describe('ship with length 3', () => {
+  let ship;
+  beforeAll(() => {
+    ship = Ship(3);
+  });
+
+  test('get length', () => {
+    expect(ship.getLength()).toBe(3);
+  });
+
+  test('does ship sink when hit at loc 2', () => {
+    ship.hit('[5, 2]');
+    expect(ship.isSunk()).toBeFalsy();
+  });
+
+  test('hit ship again at loc 1', () => {
+    ship.hit('[5, 1]');
+    expect(ship.isSunk()).toBeFalsy();
+  });
+
+  test('is ship sunk when all are hit', () => {
+    ship.hit('[5, 3]');
+    expect(ship.isSunk()).toBeTruthy();
+  });
+});
+
+describe('ship with length 2', () => {
+  let ship;
+  beforeAll(() => {
+    ship = Ship(2);
+  });
+
+  test('get length', () => {
+    expect(ship.getLength()).toBe(2);
+  });
+
+  test('does ship sink when hit at loc 2', () => {
+    ship.hit('[5, 2]');
+    expect(ship.isSunk()).toBeFalsy();
+  });
+
+  test('is ship sunk when all are hit', () => {
+    ship.hit('[5, 1]');
+    expect(ship.isSunk()).toBeTruthy();
+  });
+});
+
+describe('ship with length 1', () => {
+  let ship;
+  beforeAll(() => {
+    ship = Ship(1);
+  });
+
+  test('get length', () => {
+    expect(ship.getLength()).toBe(1);
+  });
+
+  test('is ship sunk when not hit', () => {
+    expect(ship.isSunk()).toBeFalsy();
+  });
+
+  test('is ship sunk when hit', () => {
+    ship.hit('[5, 1]');
+    expect(ship.isSunk()).toBeTruthy();
+  });
 });
