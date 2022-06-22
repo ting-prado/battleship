f34b70a9d933dba2948e5a5f15267b1f0ea32e7e
ting-prado <ethel.prado1998@gmail.com>
2022-06-22 13:50:44 +0800

    tests for viewing hitmarks added

--- tests/ship/ship.test.js
+++ tests/ship/ship.test.js
@@ -11,13 +11,17 @@ describe('ship with length 4', () => {
     expect(ship.getLength()).toBe(4);
   });
 
-  test('does ship sink when hit at loc 1', () => {
+  test('hit ship at loc 1', () => {
     ship.hit('[5, 1]');
-    expect(ship.isSunk()).toBeFalsy();
+    expect(ship.getHits()).toEqual(['x', '', '', '']);
   });
 
   test('hit ship again at loc 4', () => {
     ship.hit('[5, 4]');
+    expect(ship.getHits()).toEqual(['x', '', '', 'x']);
+  });
+
+  test('is ship sunk when not all are hit', () => {
     expect(ship.isSunk()).toBeFalsy();
   });
 
@@ -38,13 +42,17 @@ describe('ship with length 3', () => {
     expect(ship.getLength()).toBe(3);
   });
 
-  test('does ship sink when hit at loc 2', () => {
+  test('hit ship at loc 2', () => {
     ship.hit('[5, 2]');
-    expect(ship.isSunk()).toBeFalsy();
+    expect(ship.getHits()).toEqual(['', 'x', '']);
   });
 
   test('hit ship again at loc 1', () => {
     ship.hit('[5, 1]');
+    expect(ship.getHits()).toEqual(['x', 'x', '']);
+  });
+
+  test('is ship sunk when not all are hit', () => {
     expect(ship.isSunk()).toBeFalsy();
   });
 
@@ -64,8 +72,12 @@ describe('ship with length 2', () => {
     expect(ship.getLength()).toBe(2);
   });
 
-  test('does ship sink when hit at loc 2', () => {
+  test('hit ship at loc 2', () => {
     ship.hit('[5, 2]');
+    expect(ship.getHits()).toEqual(['', 'x']);
+  });
+
+  test('is ship sunk when not all are hit', () => {
     expect(ship.isSunk()).toBeFalsy();
   });
 
@@ -85,11 +97,15 @@ describe('ship with length 1', () => {
     expect(ship.getLength()).toBe(1);
   });
 
+  test('get hitmark', () => {
+    expect(ship.getHits()).toStrictEqual(['']);
+  });
+
   test('is ship sunk when not hit', () => {
     expect(ship.isSunk()).toBeFalsy();
   });
 
-  test('is ship sunk when hit', () => {
+  test('is ship sunk when all are hit', () => {
     ship.hit('[5, 1]');
     expect(ship.isSunk()).toBeTruthy();
   });
