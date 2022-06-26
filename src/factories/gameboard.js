795e52e22c672adec00b48736d9ca405b1ddb4c7
ting-prado <ethel.prado1998@gmail.com>
2022-06-26 10:35:41 +0800

    tested necessary functions for gameboard

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -1,3 +1,68 @@
-const gameboard = (() => {})();
+/* eslint-disable no-plusplus */
+const Gameboard = (() => {
+  const ships = [];
+  const missedHits = [];
+  const sunken = [];
 
-module.exports = gameboard;
+  // eslint-disable-next-line no-unused-vars
+  const addSunkenArr = () => {
+    sunken.push('');
+  };
+
+  const placeShip = (ship) => {
+    ships.push(ship);
+    addSunkenArr();
+  };
+
+  const getMissedHits = () => missedHits;
+
+  // eslint-disable-next-line consistent-return
+  const checkLost = () => {
+    let count = 0;
+    sunken.forEach((mark) => {
+      if (mark === 'x') {
+        count++;
+      }
+    });
+
+    if (count === ships.length) return true;
+  };
+
+  const checkSunken = (ship) => {
+    if (ship.isSunk()) {
+      const index = ships.indexOf(ship);
+      sunken[index] = 'x';
+    }
+  };
+
+  const receiveAttack = (coord) => {
+    let isShipHit = false;
+    ships.forEach((ship) => {
+      ship.getPos().forEach((pos) => {
+        if (pos === coord) {
+          isShipHit = true;
+          ship.hit(coord);
+          checkSunken(ship);
+        }
+      });
+    });
+
+    if (!isShipHit) {
+      missedHits.push(coord);
+    }
+
+    return isShipHit;
+  };
+
+  const getSunken = () => sunken;
+
+  return {
+    placeShip,
+    receiveAttack,
+    getMissedHits,
+    getSunken,
+    checkLost,
+  };
+})();
+
+module.exports = Gameboard;
--- src/factories/tests/gameboard.spec.js
+++ src/factories/tests/gameboard.spec.js
@@ -1,1 +1,45 @@
-const gameboard = require('../gameboard');
+/* eslint-disable one-var */
+/* eslint-disable no-undef */
+const Gameboard = require('../gameboard');
+const Ship = require('../ship');
+
+// eslint-disable-next-line one-var-declaration-per-line
+let ship1, ship2;
+beforeAll(() => {
+  ship1 = Ship(4);
+  ship2 = Ship(3);
+  ship1.setPos(['(5, 1)', '(5, 2)', '(5, 3)', '(5, 4)']);
+  ship2.setPos(['(4, 1)', '(4, 2)', '(4, 3)']);
+  Gameboard.placeShip(ship1);
+  Gameboard.placeShip(ship2);
+});
+
+test('attack hits a ship', () => {
+  expect(Gameboard.receiveAttack('(4, 2)')).toBeTruthy();
+});
+
+test('attack does not hit a ship', () => {
+  expect(Gameboard.receiveAttack('(6, 6)')).toBeFalsy();
+});
+
+test('view missed hits', () => {
+  expect(Gameboard.getMissedHits()).toEqual(['(6, 6)']);
+});
+
+test('sink ship2', () => {
+  Gameboard.receiveAttack('(4, 1)');
+  Gameboard.receiveAttack('(4, 3)');
+  expect(Gameboard.getSunken()).toEqual(['', 'x']);
+});
+
+test('sink ship1 and ship2', () => {
+  Gameboard.receiveAttack('(5, 1)');
+  Gameboard.receiveAttack('(5, 3)');
+  Gameboard.receiveAttack('(5, 2)');
+  Gameboard.receiveAttack('(5, 4)');
+  expect(Gameboard.getSunken()).toEqual(['x', 'x']);
+});
+
+test('check if game is over', () => {
+  expect(Gameboard.checkLost()).toBeTruthy();
+});
