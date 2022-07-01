0628d71bd37e92b5a89dc04d37d9ef3f7bb09531
ting-prado <ethel.prado1998@gmail.com>
2022-07-01 10:07:47 +0800

    adjusted tests for changed functions

--- src/factories/tests/gameboard.spec.js
+++ src/factories/tests/gameboard.spec.js
@@ -4,19 +4,18 @@ const Gameboard = require('../gameboard');
 const Ship = require('../ship');
 
 // eslint-disable-next-line one-var-declaration-per-line
-let ship1, ship2, gameboard;
-beforeAll(() => {
-  gameboard = Gameboard();
-  ship1 = Ship(4);
-  ship2 = Ship(3);
-  ship1.setPos(['(5, 1)', '(5, 2)', '(5, 3)', '(5, 4)']);
-  ship2.setPos(['(4, 1)', '(4, 2)', '(4, 3)']);
-  gameboard.placeShip(ship1);
-  gameboard.placeShip(ship2);
-});
+const gameboard = Gameboard();
+const ship1 = Ship(2, ['(1, 2)', '(1, 3)']);
+const ship2 = Ship(3, ['(4, 5)', '(5, 5)', '(6, 5)']);
+gameboard.placeShip(ship1);
+gameboard.placeShip(ship2);
 
 test('attack hits a ship', () => {
-  expect(gameboard.receiveAttack('(4, 2)')).toBeTruthy();
+  expect(gameboard.receiveAttack('(1, 2)')).toBeTruthy();
+});
+
+test('attack hits another ship', () => {
+  expect(gameboard.receiveAttack('(5, 5)')).toBeTruthy();
 });
 
 test('attack does not hit a ship', () => {
@@ -28,19 +27,12 @@ test('view missed hits', () => {
 });
 
 test('sink ship2', () => {
-  gameboard.receiveAttack('(4, 1)');
-  gameboard.receiveAttack('(4, 3)');
+  gameboard.receiveAttack('(4, 5)');
+  gameboard.receiveAttack('(6, 5)');
   expect(gameboard.getSunken()).toEqual(['', 'x']);
 });
 
-test('sink ship1 and ship2', () => {
-  gameboard.receiveAttack('(5, 1)');
-  gameboard.receiveAttack('(5, 3)');
-  gameboard.receiveAttack('(5, 2)');
-  gameboard.receiveAttack('(5, 4)');
+test('sink ship1', () => {
+  gameboard.receiveAttack('(1, 3)');
   expect(gameboard.getSunken()).toEqual(['x', 'x']);
-});
-
-test('check if game is over', () => {
-  expect(gameboard.checkLost()).toBeTruthy();
 });
--- src/factories/tests/ship.spec.js
+++ src/factories/tests/ship.spec.js
@@ -4,57 +4,29 @@ const Ship = require('../ship');
 const ship1 = Ship(4, ['(5, 1)', '(5, 2)', '(5, 3)', '(5, 4)']);
 const ship2 = Ship(3, ['(3, 5)', '(4, 5)', '(5, 5)']);
 
-test('get first ship info', () => {
-  expect(ship1.getInfo()).toEqual({
-    length: 4,
-    pos: ['(5, 1)', '(5, 2)', '(5, 3)', '(5, 4)'],
-    sunken: false,
-  });
+test('get first ship length', () => {
+  expect(ship1.getLength()).toBe(4);
 });
 
-test('get second ship info', () => {
-  expect(ship2.getInfo()).toEqual({
-    length: 3,
-    pos: ['(3, 5)', '(4, 5)', '(5, 5)'],
-    sunken: false,
-  });
+test('get second ship position', () => {
+  expect(ship2.getPos()).toEqual(['(3, 5)', '(4, 5)', '(5, 5)']);
 });
 
-test('check if first ship is sunk when hit once', () => {
+test('is first ship sunk when hit once', () => {
   ship1.hit('(5, 1)');
-  expect(ship1.getInfo()).toEqual({
-    length: 4,
-    pos: ['(5, 1)', '(5, 2)', '(5, 3)', '(5, 4)'],
-    sunken: false,
-  });
+  expect(ship1.isSunk()).toBeFalsy();
 });
 
-test('check if second ship is sunk when hit once', () => {
+test('is second ship sunk when all are hit', () => {
+  ship2.hit('(5, 5)');
+  ship2.hit('(3, 5)');
   ship2.hit('(4, 5)');
-  expect(ship2.getInfo()).toEqual({
-    length: 3,
-    pos: ['(3, 5)', '(4, 5)', '(5, 5)'],
-    sunken: false,
-  });
+  expect(ship2.isSunk()).toBeTruthy();
 });
 
-test('check if first ship is sunk when all are hit', () => {
+test('is first ship sunk when all are hit', () => {
   ship1.hit('(5, 2)');
   ship1.hit('(5, 3)');
   ship1.hit('(5, 4)');
-  expect(ship1.getInfo()).toEqual({
-    length: 4,
-    pos: ['(5, 1)', '(5, 2)', '(5, 3)', '(5, 4)'],
-    sunken: true,
-  });
-});
-
-test('check if first ship is sunk when all are hit', () => {
-  ship2.hit('(3, 5)');
-  ship2.hit('(5, 5)');
-  expect(ship2.getInfo()).toEqual({
-    length: 3,
-    pos: ['(3, 5)', '(4, 5)', '(5, 5)'],
-    sunken: true,
-  });
+  expect(ship1.isSunk()).toBeTruthy();
 });
