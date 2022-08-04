0e97f1d0a3421f10ef356fd2b4b9aee8ad1e51f6
ting-prado <ethel.prado1998@gmail.com>
2022-08-04 19:40:46 +0800

    separated event listeners into another function

--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 /* eslint-disable no-plusplus */
-function createContainer() {
+const createContainer = () => {
   const alphLabel = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 
   const container = document.createElement('div');
@@ -32,7 +32,31 @@ function createContainer() {
     sideSpan.textContent = i + 1;
     sideCont.appendChild(sideSpan);
   }
+};
 
+const createBlock = (length) => {
+  const size = 40.91;
+  const block = document.createElement('div');
+  block.classList.add('draggable');
+  block.draggable = true;
+  const random = Math.floor(Math.random() * 2);
+  // change the appendable grids depending on length
+  if (random === 1) {
+    block.style.width = `${size}px`;
+    block.style.height = `${size * length}px`;
+  } else {
+    block.style.width = `${size * length}px`;
+    block.style.height = `${size}px`;
+  }
+
+  return block;
+};
+
+const addFuncs = (block, orientation) => {
+  // activate these functions during dragstart
+  // get length of block that is being dragged
+  // change drop targets according to length
+  const grids = document.querySelectorAll('.grid');
   const dragEnter = (e) => {
     e.preventDefault();
     e.target.classList.add('drag-over');
@@ -57,64 +81,40 @@ function createContainer() {
     dragged.classList.remove('dragged');
   };
 
-  const grids = document.querySelectorAll('.gameboard > span');
   grids.forEach((grid) => {
     grid.addEventListener('dragenter', dragEnter);
     grid.addEventListener('dragover', dragOver);
     grid.addEventListener('dragleave', dragLeave);
     grid.addEventListener('drop', drop);
   });
-}
 
-function createBlocks() {
-  const block = document.createElement('div');
-  const btn = document.querySelector('button');
-  block.classList.add('draggable');
-  const grids = document.querySelectorAll('.grid');
-  grids[Math.floor(Math.random() * 100)].appendChild(block);
-
-  block.draggable = true;
-  // randomize either height or width gets multiplied instead of rotating
-  // length 4, vertical, left 0 - 90%; top 0 - 60%
-  // length 3, vertical, left 0 - 90%, top 0 - 70%
-  // length 2, vertical, left 0 - 90%, top 0 - 80%
-  // length 4, horizontal, left 0 - 60%, top 0 - 90%
-  // length 3, horizontal, left 0 - 70%, top 0 - 90%
-  // length 2, horizontal, left 0 - 80%, top 0 - 90%
-  // length 1, left 0 - 90%, top 0 - 90%
-  const min = Math.ceil(1); // inclusive
-  const max = Math.floor(8); // exclusive
-  // block.style.left = '10%'; // `${Math.floor(Math.random() * (max - min) + min) * 10}%`;
-  // block.style.top = '70%'; // `${Math.floor(Math.random() * (max - min) + min) * 10}%`;
+  block.addEventListener('click', () => {
+    const grid = block.parentNode;
+    const index = Array.prototype.indexOf.call(grids, grid);
+    const pos = [];
+    for (
+      let k = 0;
+      // prettier-ignore
+      k < Math.round(
+        (random === 1 ? block.offsetHeight : block.offsetWidth) / size
+      );
+      k++
+    ) {
+      pos.push(index + (random === 1 ? k * 10 : k));
+    }
+    console.log(pos.join(', '));
+  });
 
   block.addEventListener('dragstart', (e) => {
+    // add drag properties to grid on dragstart
+    // follow percentage below for grids allowed to be placed on
+    // remove drag properties on grids after dropping
+    // add checker so blocks won't overlap
     e.target.classList.add('dragged');
     setTimeout(() => {
       e.target.classList.add('hide');
     }, 0);
   });
+};
 
-  btn.addEventListener('click', () => {
-    const pos = block.getBoundingClientRect();
-    const width = Math.ceil(pos.width / 40.91);
-    const height = Math.ceil(pos.height / 40.91);
-    const posTop = Math.floor(pos.top / 40.91) - 1;
-    const posLeft = Math.floor(pos.left / 40.91) - 7;
-
-    if (width > 1 || height > 1) {
-      if (height > 1) {
-        for (let i = 0; i < height; i++) {
-          console.log(posLeft, posTop + i);
-        }
-      } else if (width > 1) {
-        for (let i = 0; i < width; i++) {
-          console.log(posLeft + i, posTop);
-        }
-      }
-    } else {
-      console.log(posLeft, posTop);
-    }
-  });
-}
-
-export { createContainer, createBlocks };
+export { createContainer, createBlock, addFuncs };
