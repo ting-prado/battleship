44d6051e486f9c26e03e897a721e62548e2e2c59
ting-prado <ethel.prado1998@gmail.com>
2022-08-04 19:41:35 +0800

    place blocks differently according to length & orientation

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
-import { createContainer, createBlocks } from './dom';
+/* eslint-disable default-case */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-unused-vars */
+import * as dom from './dom';
 import Ship from './factories/ship';
 import Player from './factories/player';
-
-window.addEventListener('load', () => {
-  createContainer();
-  createBlocks();
-});
 
 // main game loop
 // starts with creating players & populate each gameboard
@@ -18,8 +16,88 @@ window.addEventListener('load', () => {
 // using only function inside the game loop
 // create conditions so that the game ends once
 // one player's ships have all been sunk
+const gameFunc = (() => {
+  const checkAvailPos = (length, orientation) => {
+    const arr = [];
+    if (orientation === 'portrait') {
+      switch (length) {
+        case 4:
+          for (let i = 0; i < 70; i++) {
+            arr.push(i);
+          }
+          break;
+        case 3:
+          for (let i = 0; i < 80; i++) {
+            arr.push(i);
+          }
+          break;
+        case 2:
+          for (let i = 0; i < 90; i++) {
+            arr.push(i);
+          }
+          break;
+        case 1:
+          for (let i = 0; i < 100; i++) {
+            arr.push(i);
+          }
+          break;
+      }
+    } else {
+      let limits;
+      switch (length) {
+        case 4:
+          limits = [7, 8, 9];
+          break;
+        case 3:
+          limits = [8, 9];
+          break;
+        case 2:
+          limits = [9];
+          break;
+      }
+      for (let i = 0; i < 100; i++) {
+        const numStr = i.toString();
+        let avail = true;
+        limits.forEach((num) => {
+          if (i === num || numStr[1] == num) {
+            avail = false;
+          }
+        });
+        if (avail) {
+          arr.push(i);
+        }
+      }
+    }
+    return arr;
+  };
 
-const gameLoop = (() => {
-  const ai = Player('comp');
-  const human = Player();
+  const createPlayerShips = (player) => {
+    if (player.type === 'human') {
+      const grids = document.querySelectorAll('.grid');
+      let length = 4;
+      let count = 1;
+      for (let i = 0; i < 4; i++) {
+        for (let k = 0; k < count; k++) {
+          const block = dom.createBlock(length);
+          // prettier-ignore
+          const orientation = block.style.width.match(/^.+?(?=px)/)[0] / 40.91 > 1
+            ? 'landscape'
+            : 'portrait';
+          const options = checkAvailPos(length, orientation);
+          grids[
+            options[Math.floor(Math.random() * options.length)]
+          ].appendChild(block);
+        }
+        length--;
+        count++;
+      }
+    }
+  };
+
+  const init = (() => {
+    dom.createContainer();
+    const human = Player();
+    const ai = Player('comp');
+    createPlayerShips(human);
+  })();
 })();
